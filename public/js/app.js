ffd7ef28044fd8a2b2b970895889711312199f65
CarlosSu02 <carlossuplei@gmail.com>
2021-12-25 23:33:26 -0600

    actualizacion y envio de contacto con formsubmit y sweetalert2

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,8 @@ var cars = new Array(
   {image: "./img/22r1988.jpg", mark: "Toyota", model: "22r", year: "1988", version: "Sencillo"},
   {image: "./img/22r19882.jpg", mark: "Toyota", model: "22r", year: "1988", version: "Sencillo"},
   {image: "./img/corolla1990.jpg", mark: "Toyota", model: "Corolla", year: "1990", version: "Normal"},
-  {image: "./img/tercel1991.jpg", mark: "Toyota", model: "Tercel", year: "1991", version: "Cabina Sencilla"},
-  {image: "./img/corolla1995.webp", mark: "Toyota", model: "Corolla", year: "1995", version: "Normal"},
+  {image: "./img/tercel1996.jpg", mark: "Toyota", model: "Tercel", year: "1996", version: "Cabina Sencilla"},
+  {image: "./img/corolla1993.webp", mark: "Toyota", model: "Corolla", year: "1993", version: "Normal"},
   {image: "./img/ranger2000.png", mark: "Ford", model: "Ranger", year: "2000", version: "Cabina y Media Paila Ranchera"},
   {image: "./img/cheyenne1995.jpg", mark: "Chevrolet", model: "Cheyenne", year: "1995", version: "Cabina y Media"}, 
   {image: "./img/montero19892.jpg", mark: "Mitsubichi", model: "Montero", year: "1989", version: "2 puertas"}, 
@@ -51,3 +51,99 @@ function WriteCars(){
 }
 
 WriteCars();
+
+function executeExample(emailInput){
+	
+  // Swal.fire({title: "Hola!"});
+
+  // const { value: email } = await Swal.fire({
+  //   title: 'Input email address',
+  //   input: 'email',
+  //   inputLabel: 'Your email address',
+  //   inputPlaceholder: 'Enter your email address'
+  // })
+  
+  // if (email) {
+  //   Swal.fire(`Entered email: ${email}`)
+  // }
+
+  (async () => {
+
+    const { value: name } = await Swal.fire({
+      title: 'Input name',
+      input: 'text',
+      inputLabel: 'Your name',
+      inputPlaceholder: 'Enter your name',
+      name: "name"
+    })
+
+    const { value: email } = await Swal.fire({
+      title: 'Input email address',
+      input: 'email',
+      inputLabel: 'Your email address',
+      inputPlaceholder: 'Enter your email address',
+      name: "email"
+    })
+
+    const { value: comments } = await Swal.fire({
+      input: 'textarea',
+      inputLabel: 'Message',
+      inputPlaceholder: 'Type your message here...',
+      inputAttributes: {
+        'aria-label': 'Type your message here'
+      },
+      showCancelButton: true,
+      name: "text"
+    })
+     
+    // Swal.fire(`Entered email: ${email} - ${name} - ${text}`);
+    Swal.fire({
+      
+      html:
+      `<form action="https://formsubmit.co/saberparaque11@gmail.com" method="POST">
+      <!-- <input type="text" name="name" required>
+        <input type="email" name="email" required>
+        <br><br><textarea name="comments" rows="5" cols="50" placeholder="Mensaje" required></textarea>
+        <input type="text" name="name" value="${name}" required>
+        <input type="email" name="email" value="${email}" required>
+        <br><br><textarea value="${comments}" rows="5" cols="50" placeholder="Mensaje" required>${comments}</textarea> -->
+        <input type="hidden" name="email" value="${email}">
+        <input type="hidden" name="name" value="${name}">
+        <input type="hidden" name="comments" value="${comments}">
+        <br><button type="submit">Send</button>
+      </form>`
+
+    });  
+  })();
+  // Swal.fire({
+    
+  //   html:
+  //   `<label>Su nombre: <input type="text" autofocus placeholder="Nombre"><label>
+  //    <label>Su email: <input type="email" autofocus placeholder="Email"><label>
+  //    <textarea id="swal-input2" rows="10" cols="30" class="swal2-input" placeholder="Mensaje">`
+
+  // });
+
+  // var name = "Prueba1";
+  // var email = "saberparaque1@gmail.com";
+  // var comments = "prueba de correo 13";
+
+  // Swal.fire({
+    
+  //   html:
+  //   `<form action="https://formsubmit.co/saberparaque11@gmail.com" method="POST">
+  //   <!-- <input type="text" name="name" required>
+  //     <input type="email" name="email" required>
+  //     <br><br><textarea name="comments" rows="5" cols="50" placeholder="Mensaje" required></textarea>
+  //     <input type="text" name="name" value="${name}" required>
+  //     <input type="email" name="email" value="${email}" required>
+  //     <br><br><textarea value="${comments}" rows="5" cols="50" placeholder="Mensaje" required>${comments}</textarea> -->
+  //     <input name="email" value="${email}">
+  //     <input name="name" value="${name}">
+  //     <input name="comments" value="${comments}">
+  //     <br><button type="submit">Send</button>
+  //   </form>`
+
+  // });  
+
+}
