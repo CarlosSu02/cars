c35d7306888178ed783fe65e8475572d04d1e304
CarlosSu02 <carlossuplei@gmail.com>
2021-12-28 16:30:18 -0600

    implementando barNav

--- js/navBar.js
+++ js/navBar.js
@@ -1,25 +1,19 @@
 
-// alert("Footer en prueba");
-
 posicionarMenu();
- 
+
 $(window).scroll(function() {    
     posicionarMenu();
 });
 
 function posicionarMenu() {
     var altura_del_header = $('header').outerHeight(true);
-    // var altura_del_menu = $('nav').outerHeight(true);
-
-    alert("As");
+    var altura_del_menu = $('.nav').outerHeight(true);
 
     if ($(window).scrollTop() >= altura_del_header){
-        // $('fixed')
-        $('.bar').addClass('fixed');
-        // $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
+        $('.nav').addClass('fixed');
+        $('.section').css('margin-top', (altura_del_menu) + 'px');
     } else {
-        // $('#nav');
-        $('.bar').removeClass('fixed');
-        // $('.wrapper').css('margin-top', '0');
+        $('.nav').removeClass('fixed');
+        $('.section').css('margin-top', '0');
     }
 }
