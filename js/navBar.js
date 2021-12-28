0f5d96ee46d2ca6b7d64fa66302e63c17aa465ff
CarlosSu02 <carlossuplei@gmail.com>
2021-12-28 13:52:30 -0600

    prueba con el nav 8

--- js/navBar.js
+++ js/navBar.js
@@ -11,6 +11,8 @@ function posicionarMenu() {
     var altura_del_header = $('header').outerHeight(true);
     // var altura_del_menu = $('nav').outerHeight(true);
 
+    alert("As");
+
     if ($(window).scrollTop() >= altura_del_header){
         // $('fixed')
         $('.bar').addClass('fixed');
